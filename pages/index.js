ae723b4d66ccef89ba94bb52c2cfbdd281087e79
Selcuk <105418729+Qucles@users.noreply.github.com>
2023-10-15 00:02:01 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,14 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React from 'react';
+import Link from 'next/link';
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+    <div>
+      <h1>Ana Sayfa</h1>
+      <p>Hoş geldiniz!</p>
+      <Link href="/altSayfa">
+        <a>Git: Alt Sayfa</a>
+      </Link>
     </div>
-  )
+  );
 }
